00232c6ecd5ee66f2bda0bb244e63ccc7ac0d4b8
yishenghuang <406390171@qq.com>
2020-03-16 22:22:23 +0800

    Update HelloWorld.java

--- src/com/ysae/HelloWorld.java
+++ src/com/ysae/HelloWorld.java
@@ -3,7 +3,7 @@ package com.ysae;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World2");
-		System.out.println("Hello World3");
+		System.out.println("Hello World666");
 	}
 
 }
