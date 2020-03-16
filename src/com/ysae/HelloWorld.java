72496fcb819e5f070c9e3fbbc40e31c3dfe39e1e
ysae_h <ysae_h@192.168.56.1>
2020-03-16 21:42:50 +0800

    555

--- src/com/ysae/HelloWorld.java
+++ src/com/ysae/HelloWorld.java
@@ -2,10 +2,7 @@ package com.ysae;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello World2");
-=======
->>>>>>> branch 'master' of https://github.com/yishenghuang/HelloWorld.git
 		System.out.println("Hello World3");
 	}
 
